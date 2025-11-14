820768f0d9792cede36748fd4f953cd63b30f4a7
mostafamahmoud11911 <mm0766633@gmail.com>
2025-11-15 02:21:58 +0400

    edit

--- src/modules/auth/authController.ts
+++ src/modules/auth/authController.ts
@@ -36,7 +36,7 @@ export const login = createError(
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: true,
+        secure: false,
         sameSite: "none",
         path: "/",
         maxAge: 7 * 24 * 60 * 60 * 1000,
