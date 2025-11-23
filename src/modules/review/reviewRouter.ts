64414cb07d3549f495d7576a6d67a4f6cb8017fe
mostafamahmoud11911 <mm0766633@gmail.com>
2025-11-23 20:40:05 +0400

    Update Review Route

--- src/modules/review/reviewRouter.ts
+++ src/modules/review/reviewRouter.ts
@@ -7,10 +7,10 @@ import { addReviewValidation, deleteReviewValidation, getReviewValidation } from
 
 const reviewRouter = express.Router();
 
-reviewRouter.post("/", validate(addReviewValidation), auth, authGuard("user"), createReview);
+reviewRouter.post("/all/", validate(addReviewValidation), auth, authGuard("user"), createReview);
 reviewRouter.get("/all/:id", getServiceReviews)
-reviewRouter.get("/:id", validate(getReviewValidation), auth, authGuard("user"), getServiceReview);
-reviewRouter.delete("/:id", validate(deleteReviewValidation), auth, authGuard("user", "provider", "admin"), deleteReview)
+reviewRouter.get("/all/:id", validate(getReviewValidation), auth, authGuard("user"), getServiceReview);
+reviewRouter.delete("/all/:id", validate(deleteReviewValidation), auth, authGuard("user", "provider", "admin"), deleteReview)
 
 
 export default reviewRouter;
